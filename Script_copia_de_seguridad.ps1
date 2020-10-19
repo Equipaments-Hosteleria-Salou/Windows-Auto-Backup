d84630b95905c6a93d5c87d334627233451d67a8
Aleix Mariné Tena <aleix.marine@estudiants.urv.cat>
2020-10-19 14:50:00 +0200

    added parametrized varis

--- Script_copia_de_seguridad.ps1
+++ Script_copia_de_seguridad.ps1
@@ -1,24 +1,26 @@
 
 # Destination folder
-$Destination = "C:\COPIA_SEGURETAT\COPIA_SEGURETAT_$(Get-Date -f dd_MM_yyyy)"
+$BackupFolder = "C:\COPIA_SEGURETAT"
+$tgData = "\\servidornou\dades\TGProfesional"
+$Destination = "$BackupFolder\COPIA_SEGURETAT_$(Get-Date -f dd_MM_yyyy)"
 echo "LA COPIA DE SEGURIDAD AUTOMATICA VA A EMPEZAR. NO CIERRES ESTA VENTANA"
 
 # Checks if the backup folder exists
 if (Test-Path -Path 'C:\COPIA_SEGURETAT' -PathType Container)
 {
     # Obtain the name of the last backup
-	$oldcopyname = "$(dir c:\COPIA_SEGURETAT)"
+	$oldcopyname = "$(dir $BackupFolder)"
     # Copy TG data to the backup folder
-	Copy-Item -Recurse -Path \\servidornou\dades\TGProfesional -Destination $Destination
+	Copy-Item -Recurse -Path $tgData -Destination $Destination
     # Remove old backup
-	Remove-Item –path c:\COPIA_SEGURETAT\$oldcopyname
+	Remove-Item –path $BackupFolder\$oldcopyname
 }
 else  # If the backup folder does not exist
 {
     # Create backup folder
 	New-Item -Path "c:\" -Name "COPIA_SEGURETAT" -ItemType "directory"
     # Copy TG data to the backup folder
-	Copy-Item -Recurse -Path \\servidornou\dades\TGProfesional -Destination $Destination
+	Copy-Item -Recurse -Path $tgData -Destination $Destination
 }
 echo "COPIA FINALIZADA"
 echo "PULSA CUALQUIER TECLA PARA SALIR"
