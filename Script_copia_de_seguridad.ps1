56d66471cc5feb49ce4604d833585aa4c1220a58
Aleix Mariné Tena <aleixaretra@gmail.com>
2020-10-21 15:58:59 +0200

    Update Script_copia_de_seguridad.ps1

--- Script_copia_de_seguridad.ps1
+++ Script_copia_de_seguridad.ps1
@@ -2,7 +2,7 @@
 # Destination folder
 $BackupFolder = "C:\COPIA_SEGURETAT"
 $tgData = "\\servidornou\dades\TGProfesional"
-$Destination = "$BackupFolder\COPIA_SEGURETAT_$(Get-Date -f dd_MM_yyyy).zip"
+$Destination = "$BackupFolder\COPIA_SEGURETAT_$(Get-Date -f dd_MM_yyyy)"
 
 echo "LA COPIA DE SEGURIDAD AUTOMATICA VA A EMPEZAR. NO CIERRES ESTA VENTANA"
 
