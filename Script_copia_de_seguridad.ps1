2f0224b4a18d161b517b369801ae9a1df8e3f0a7
Aleix Mariné Tena <aleixaretra@gmail.com>
2020-10-21 15:10:21 +0200

    CHANGED THINGS

--- Script_copia_de_seguridad.ps1
+++ Script_copia_de_seguridad.ps1
@@ -9,24 +9,26 @@ echo "LA COPIA DE SEGURIDAD AUTOMATICA VA A EMPEZAR. NO CIERRES ESTA VENTANA"
 # Checks if the backup folder exists
 if (Test-Path -Path 'C:\COPIA_SEGURETAT' -PathType Container)
 {
-    # Obtain the name of the last backup
-	$oldcopyname = "$(dir $BackupFolder)"
-    # Copy TG data to the backup folder
-    Compress-Archive -Path $tgData\* -DestinationPath $Destination
-	#Copy-Item -Recurse -Path $tgData -Destination $Destination
-    # Remove old backup
-	Remove-Item –path $BackupFolder\$oldcopyname
+    # Obtain the names of the last backup
+    $backupList = Get-ChildItem -Path C:\COPIA_SEGURETAT 
+    Copy-Item -Recurse -Path $tgData -Destination $Destination
+    foreach ($backupfolder in $backupList) 
+    {
+        $backupfolderPath = $backupfolder.fullname
+        # Remove old backup
+	    Remove-Item â€“path $backupfolderPath -Recurse
+    }
 }
 else  # If the backup folder does not exist
 {
     # Create backup folder
 	New-Item -Path "c:\" -Name "COPIA_SEGURETAT" -ItemType "directory"
     # Copy TG data to the backup folder
-    Compress-Archive -Path $tgData\* -DestinationPath $Destination
-	#Copy-Item -Recurse -Path $tgData -Destination $Destination
+	Copy-Item -Recurse -Path $tgData -Destination $Destination
 }
 
 echo "COPIA FINALIZADA"
 echo "PULSA CUALQUIER TECLA PARA SALIR"
 
 [Console]::ReadKey()
+
